aaa6b2248984631f1aefa50339e68febc6356fce
Dima Samchuk <dvsamchuk@gmail.com>
2020-07-19 08:17:25 +0300

    sdist built ok

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='Python utils for data science competitions',
+    name='dshortcuts',
     version='0.0.1',
-    long_description='Here are a lot of useful funcitons which are actually shortcuts to save the time on opening files, cache, transforming data, etc',
-    url='https://github.com/Codealist/dshortcuts'
+    long_description='Data science shortcuts is a set of useful funcitons to save the time on opening files, cache, transforming data, etc',
+    url='https://github.com/Codealist/dshortcuts',
+    author='Dmytro Samchuk',
+    author_email='dvsamchuk@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['pickle'],
+    python_requires='>=3.5'
 )
